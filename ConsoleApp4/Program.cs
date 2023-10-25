2600e1b8c61ea77258d9142534c0bf17bc5c3f62
СемёнТихонов <@LAPTOP-G0BRQBKQ>
2023-10-25 15:56:32 +0200

    heuristics

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Triplet
 {
@@ -10,47 +13,118 @@ namespace Triplet
     {
         static void Main(string[] args)
         {
-            var filePath = "C:\\test.txt";
-            GetTopTenTriplet(filePath);
+            var filePath = "C:\\test1.txt";
+            long fileSize;
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                fileSize = fileStream.Length;
+            }
+            //bufferSize зависит от размера файла, и количества памяти
+            var bufferSize = 1024 * 1024;
+            var codersWords = CreateMapLetters();
+            Bench(() => ProcessFileInParalle(filePath, bufferSize, codersWords, fileSize));
         }
 
-        private static void GetTopTenTriplet(string filePath)
+        static Dictionary<char, int> CreateMapLetters()
+        {
+            var count = 0;
+            var codersWords = new Dictionary<char, int>();
+
+            for (var letter = 'a'; letter <= 'z'; letter++)
+            {
+                codersWords.Add(letter, count);
+                count++;
+            }
+
+            for (var letters = 'A'; letters <= 'Z'; letters++)
+            {
+                codersWords.Add(letters, count);
+                count++;
+            }
+            return codersWords;
+        }
+
+        static void Bench(Action action)
         {
             var stp = new Stopwatch();
             stp.Start();
-
-            var fileContent = File.ReadAllText(filePath);
-            var chunks = fileContent.Length / Environment.ProcessorCount;
-            var chunkSize = chunks > 3 ? chunks : fileContent.Length;
-            var triplet = new ConcurrentDictionary<string, int>();
-            fileContent.Chunk(chunkSize)
-                .AsParallel()
-                .WithDegreeOfParallelism(Environment.ProcessorCount)
-                .ForAll(x => AddTriplet(x, triplet));
-
-            var topTenTriplet = triplet.OrderBy(x => x.Value).TakeLast(10);
-            foreach (var x in topTenTriplet)
-            {
-                Console.WriteLine((x.Key, x.Value));
-            }
+            action();
             stp.Stop();
             Console.WriteLine(stp.ElapsedMilliseconds);
         }
 
-        private static void AddTriplet(char[] str, ConcurrentDictionary<string, int> triplet)
+        static void ProcessFileInParalle(string filePath, int bufferSize, Dictionary<char, int> codersWords, long fileSize)
         {
-            var tmp = 0;
-            for (int i = 3; i <= str.Length; i++)
-            {
-                var stopWords = new char[] { ' ', '!', '?', '.' };
-                if (str[tmp..i].Any(x => stopWords.Contains(x)))
+            var allCountTriplets = 52 * 52 * 52;
+            var allTriplet = new int[allCountTriplets];
+            var partitioner = Partitioner.Create(0, fileSize, bufferSize);
+            Parallel.ForEach(partitioner, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+                range =>
                 {
-                    tmp++;
-                    continue;
-                }
-                triplet.AddOrUpdate(new string(str[tmp..i]), key => 1, (key, existingValue) => existingValue + 1);
-                tmp++;
+                    var localArrayLetters = new int[allCountTriplets];
+
+                    using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        fileStream.Seek(range.Item1, SeekOrigin.Begin);
+
+                        while (fileStream.Position < range.Item2)
+                        {
+                            var buffer = new byte[bufferSize];
+                            var bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+
+                            if (bytesRead == 0)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                var tripletStart = 0;
+                                var result = Encoding.UTF8.GetString(buffer, 0, bytesRead).Replace("\0", "");
+                                for (var tripletEnd = 3; tripletEnd <= result.Length; tripletEnd++, tripletStart++)
+                                {
+                                    var triplet = result[tripletStart..tripletEnd];
+                                    if (triplet.All(x => codersWords.ContainsKey(x)))
+                                    {
+                                        var codersTriplet = codersWords[result[tripletStart]] * 52 * 52
+                                        + codersWords[result[tripletStart + 1]] * 52
+                                        + codersWords[result[tripletStart + 2]];
+                                        localArrayLetters[codersTriplet]++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    lock (allTriplet)
+                    {
+                        for (var i = 0; i < localArrayLetters.Length; i++)
+                        {
+                            allTriplet[i] += localArrayLetters[i];
+                        }
+                    }
+                });
+
+            var topTenTriplets = allTriplet
+                .Select((value, index) => new { value, index })
+                .OrderByDescending(item => item.value)
+                .Take(10)
+                .ToArray();
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.Write(topTenTriplets[i].value);
+                var first = topTenTriplets[i].index / (52 * 52);
+                var second = (topTenTriplets[i].index - first * 52 * 52) / 52;
+                var third = topTenTriplets[i].index - (first * 52 * 52 + second * 52);
+                var leterOne = codersWords.First(x => x.Value == first);
+                var leterTwo = codersWords.First(x => x.Value == second);
+                var leterThree = codersWords.First(x => x.Value == third);
+                Console.WriteLine((leterOne.Key, leterTwo.Key, leterThree.Key));
             }
         }
     }
 }
+
+
+
+
